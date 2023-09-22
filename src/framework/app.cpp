167101e52bf98aff696bb0ee2d6b3d06da923e50
Julian C. Stamm <mail@julcs.de>
2023-09-22 09:47:20 +0200

    Upgrade to C++17, OpenGL 4.1

--- src/framework/app.cpp
+++ src/framework/app.cpp
@@ -30,8 +30,8 @@ void App::initGLFW() {
     assert(glfwInitStatus);
 
     // Window hints
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
