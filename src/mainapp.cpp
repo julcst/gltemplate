5b1438f07d1c8167cae5d16b8246ce6dcb612eb1
Julian C. Stamm <juliancstamm@gmail.com>
2024-04-30 11:18:13 +0200

    Inculde overrides

--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -2,28 +2,13 @@
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <imgui.h>
-
-#include <vector>
-#include <iostream>
+using namespace glm;
 
 #include "config.hpp"
-
-using namespace glm;
 
 MainApp::MainApp() : App(800, 600) {
     App::setTitle(Config::PROJECT_NAME);
     // Allocate resources
-}
-
-void MainApp::init() {
-    // Initialize
-}
-
-void MainApp::render() {
-    // Render
 }
 
 void MainApp::keyCallback(Key key, Action action) {
--- src/mainapp.hpp
+++ src/mainapp.hpp
@@ -10,7 +10,12 @@ class MainApp : public App {
     MainApp();
 
    protected:
-    void init() override;
-    void render() override;
+    // void init() override;
+    // void render() override;
+    // void buildImGui() override;
     void keyCallback(Key key, Action action) override;
+    // void clickCallback(Button button, Action action, const vec2& position) override;
+    // void scrollCallback(float amount) override;
+    // void moveCallback(const vec2& movement, bool leftButton, bool rightButton, bool middleButton) override;
+    // void resizeCallback(const vec2& resolution) override;
 };
