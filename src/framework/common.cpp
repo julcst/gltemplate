97fe5c67eaab2c8ec2396a6dccbc1a69fbaced30
Julian C. Stamm <juliancstamm@gmail.com>
2024-04-30 10:38:57 +0200

    Overwrite working directory in DEBUG mode

--- src/framework/common.cpp
+++ src/framework/common.cpp
@@ -5,28 +5,34 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 #include "config.hpp"
 
 std::string Common::readFile(const std::string& filepath) {
-    std::filesystem::path path(Config::RES_DIR + filepath);
-    std::ifstream stream(path);
-    if (!stream.is_open()) throw std::runtime_error("Could not open file: " + path.string() + " relative to current working directory: " + std::filesystem::current_path().string());
+    Config::setWorkingDirectory();
+    std::filesystem::path path{filepath};
+    std::ifstream stream{path};
+    std::cout << "Loading " << std::filesystem::absolute(path) << std::endl;
+    if (!stream.is_open()) throw std::runtime_error("Could not open file: " + std::filesystem::absolute(path).string());
     std::stringstream buffer;
     buffer << stream.rdbuf();
     return buffer.str();
 }
 
 void Common::writeToFile(const std::string& content, const std::string& filepath) {
-    std::filesystem::path path(Config::RES_DIR + filepath);
+    Config::setWorkingDirectory();
+    std::filesystem::path path{filepath};
     std::filesystem::create_directories(path.parent_path());
-    std::ofstream out(path);
-    if (!out.is_open()) throw std::runtime_error("Could not open file: " + path.string() + " relative to current working directory: " + std::filesystem::current_path().string());
+    std::ofstream out{path};
+    std::cout << "Writing " << std::filesystem::absolute(path) << std::endl;
+    if (!out.is_open()) throw std::runtime_error("Could not open file: " + std::filesystem::absolute(path).string());
     out << content;
     out.close();
 }
 
 void Common::filesInDirectory(const std::string& directoryPath, const std::string& extension, std::vector<std::string>& filenames) {
+    Config::setWorkingDirectory();
     if (std::filesystem::exists(directoryPath) && std::filesystem::is_directory(directoryPath)) {
         for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
             if (std::filesystem::is_regular_file(entry.status()) && entry.path().extension() == extension) {
